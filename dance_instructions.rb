75b226a7a2b530a5301a285359847e65b8de4b15
valeriacruise <valeriacruise22@gmail.com>
2018-07-09 20:44:19 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,27 @@
 
-# Code your methods below
+def starting_stance
+  puts "Plant legs far apart"
+  puts "bend knees slightly and keep posture loose"
+end
+
+def skip_step(step)
+  puts "Lower"
+  puts "Bounce"
+  puts "Briefly"
+end
+
+def base_footwork
+  puts "lift and return right foot"
+end
+
+
+def footwork
+  puts ""
+end
+
+def bob_the_reins 
+  puts "Holding your arms out in front of you, straight and at chest level"
+  puts "Cross your right wrist over your left and hold them together"
+  puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song"
+end
+
